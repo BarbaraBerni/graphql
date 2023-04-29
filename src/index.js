da31460155a7031e53ee981af124fa8408f5ff4f
Bárbara Berni <128430416+BarbaraBerni@users.noreply.github.com>
2023-04-28 21:39:43 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,74 @@
+
+import { createSchema, createYoga } from 'graphql-yoga'
+import { createServer } from 'node:http'
+
+const schema = createSchema({
+  typeDefs: `
+    type Query {
+      hello: String!
+      name: String!
+      id: ID!
+      location: String!
+      age: Int!
+      ofAge: Boolean!
+      salary: Float
+      EffectiveJava: Livro!
+      bemVindo (nome: String) : String!
+      notas: [Int!]!
+      somaNumeros(numeros: [Float!]): Float!
+    }
+    type Livro {
+      id: ID!
+      titulo: String!
+      genero: String!
+      edicao: Int
+      preco: Float
+    }
+  `,
+  resolvers: {
+    Query: {
+      hello(){
+        return "Minha primeira API GraphQL!"
+      },
+      name(){
+        return "Ana"
+      },
+      id: () => "uma chave qualquer aqui",
+      location: () => "Rua B, numero 1",
+      age: () => 1,
+      ofAge: () => true,
+      salary: () => 400,
+      EffectiveJava() {
+        return {
+          id: '123456',
+          titulo: 'Effective Java',
+          genero: 'Linguagem de Programacao',
+          edicao: 4,
+          preco: 112.50
+        }
+      },
+       bemVindo(parent, args, ctx, info) {
+      //   console.log ("parent: " + JSON.stringify(parent))
+      //   console.log("args: " + JSON.stringify(args))
+      //   console.log("ctx: " + (ctx))
+      //   console.log("info: " + JSON.stringify(info))
+
+      return `
+      Bem vindo ${args.nome ? args.nome :
+      'visitante'}
+      `;
+      },
+      notas(parent, args, ctx, info) {
+        return [1, 2, 3, 4]
+      }
+    }
+  }
+})
+
+const yoga = createYoga({
+  schema: schema
+})
+
+const porta = 4000
+const server = createServer(yoga)
+server.listen(porta, () => console.log(`Servidor ok. Porta ${porta}.`))
